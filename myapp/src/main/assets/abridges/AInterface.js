2b63ada466d34b626335f86c1de1c07daa86a9f1
denghui <denghui_youx@163.com>
2016-08-25 21:03:11 +0800

    delete demo

--- myapp/src/main/assets/abridges/AInterface.js
+++
@@ -1,9 +0,0 @@
-function AInterface() {}
-
-AInterface.showToastMessage = function(text) {
-  ABridgeS.call('Android', arguments);
-};
-
-AInterface.getUserInfoFromApp = function(text){
-    ABridgeS.callWithCallback('Android', arguments);
-};
